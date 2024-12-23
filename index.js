1666d16ab9c64577a12a8e8812b0f43ef1c7a32c
JSCRIPT-A-LOT <jscript.a.lot@gmail.com>
2024-12-22 21:03:19 -0700

    added footer partial

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const { v4: uuidv4 } = require('uuid');
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
